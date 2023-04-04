2433aecdfd7716b0d36b922115d4b86a1b4494ba
Souravcv <souravcv321@gmail.com>
2023-04-04 13:10:38 +0530

    validation

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,7 @@
-
-
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:email_validator/email_validator.dart';
 
 class LoggedinWidget extends StatelessWidget {
   const LoggedinWidget({super.key});
@@ -12,29 +9,166 @@ class LoggedinWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
+    final formKey = GlobalKey<FormState>();
+   
+
     return Scaffold(
-      appBar: AppBar(
-        title:const Text("Hello"),
-        centerTitle: true,
-        actions: [
-          TextButton(onPressed: (){}, child:const Text('Logout'))
-        ],
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        color: Colors.blueGrey.shade900,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("666",style: TextStyle(fontSize: 24),),
-            SizedBox(height: 32,),
-           CircleAvatar(
-            radius: 40,
-            backgroundImage: NetworkImage("https://imgs.search.brave.com/xp38TOFN041Tckx-upE6pZVSm-BhJtNiUaEt3Ao37Yc/rs:fit:459:225:1/g:ce/aHR0cHM6Ly90c2Uy/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC5aQXFOUlAt/cDcweHZlS1ZSSnQ1/OTVRSGFIcCZwaWQ9/QXBp"),
-           )
-          ],
+        appBar: AppBar(
+          title: const Text("Home"),
+          centerTitle: true,
+          backgroundColor: Colors.black,
         ),
-      ),
-    );
+        body: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                //name
+                TextFormField(
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.9),
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    labelText: "name",
+                    labelStyle: TextStyle(
+                        color: const Color.fromARGB(255, 114, 128, 128)
+                            .withOpacity(0.9)),
+                    filled: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    fillColor:
+                        const Color.fromARGB(255, 26, 190, 114).withOpacity(0.3),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          const BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[a-z A-Z]+$').hasMatch(value)) {
+                      return "Enter Correct name";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+          
+                //Email
+                TextFormField(
+                 
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.9),
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_rounded),
+                    labelText: "Email",
+                    labelStyle: TextStyle(
+                        color: const Color.fromARGB(255, 114, 128, 128)
+                            .withOpacity(0.9)),
+                    filled: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    fillColor:
+                        const Color.fromARGB(255, 26, 190, 114).withOpacity(0.3),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          const BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                   validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}').hasMatch(value)) {
+                      return "Enter Correct email";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+          
+                SizedBox(
+                  height: 10,
+                ),
+                //phone
+          
+                TextFormField(
+                  keyboardType: TextInputType.phone,
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.9),
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.phone),
+                    labelText: " Phone",
+                    labelStyle: TextStyle(
+                        color: const Color.fromARGB(255, 114, 128, 128)
+                            .withOpacity(0.9)),
+                    filled: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    fillColor:
+                        const Color.fromARGB(255, 26, 190, 114).withOpacity(0.3),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          const BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty 
+                            ) {
+                      return "Enter Correct phone Number";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+          
+                //Password
+                TextFormField(
+                  
+                  obscureText: true,
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.9),
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.password_outlined),
+                    labelText: " Password",
+                    labelStyle: TextStyle(
+                        color: const Color.fromARGB(255, 114, 128, 128)
+                            .withOpacity(0.9)),
+                    filled: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    fillColor:
+                        const Color.fromARGB(255, 26, 190, 114).withOpacity(0.3),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          const BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+          ElevatedButton(onPressed: (){
+            if (formKey.currentState!.validate()) {
+              final snackbar = SnackBar(content: Text("okkkkkk"));
+              print("jsdvfjsvfjsvjdv,jhfb fdv d");
+            }
+          },
+           child:const Text("click"))
+            
+              ],
+            ),
+          ),
+        )));
   }
 }
